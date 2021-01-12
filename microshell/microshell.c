f7bc2d674301c443b01a655df34835f547930c8c
Kronx12 <kronx12pro@gmail.com>
2021-01-12 18:28:13 +0100

    Patch strlen

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   microshell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbaud <gbaud@42lyon.fr>                    +#+  +:+       +#+        */
+/*   By: dev <dev@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 15:59:05 by gbaud             #+#    #+#             */
-/*   Updated: 2021/01/10 13:24:57 by gbaud            ###   ########lyon.fr   */
+/*   Updated: 2021/01/12 18:16:29 by dev              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 int ft_strlen(char *str) {
 	int i = 0;
 	while (str[i++]);
-	return (i);
+	return (i - 1);
 }
 
 int put_err(char *err, char *path) {
@@ -55,37 +55,35 @@ int main(int ac, char **av, char **env) {
 	int i=1,j,k,l;
 	while (i < ac) {
 		j=i, k=i, l=i;
-		while (j < ac && strcmp(av[j], ";")) // j = limite semicolon
+		while (j < ac && strncmp(av[j], ";", 2)) // j = limite semicolon
 			j++;
 		int   p[2];
 		pid_t pid;
 		int   fd_in = 0;
 		while (k < j) { // Tant qu'il y a des pipes
 			l=k;
-			while (l < j && strcmp(av[l], "|")) // [k - l] -> troncon cmd + arg
+			while (l < j && strncmp(av[l], "|", 2)) // [k - l] -> troncon cmd + arg
 				l++;
 			char *argv[l - k + 1];
 			sub(argv, av, k, l);
-			if (!strcmp(argv[0], "cd"))
-				cd(argv, l-k);
-			else {
-				pipe(p);
-				if ((pid = fork()) == -1)
-					return (put_err(ERR_FATAL, NULL));
-				else if (pid == 0) {
-					dup2(fd_in, 0);
-					if (l < j)
-						dup2(p[1], 1);
-					close(p[0]);
-					if (execve(argv[0], argv, env))
-						put_err(ERR_EXEC, argv[0]);
-					exit(EXIT_FAILURE);
-				} else {
-					waitpid(pid, NULL, 0);
-					close(p[1]);
-					fd_in = p[0];
-        		}
-			}
+			pipe(p);
+			if ((pid = fork()) == -1)
+				return (put_err(ERR_FATAL, NULL));
+			else if (pid == 0) {
+				dup2(fd_in, 0);
+				if (l < j)
+					dup2(p[1], 1);
+				close(p[0]);
+				if (!strncmp(argv[0], "cd", 3))
+					cd(argv, l-k);
+				else if (execve(argv[0], argv, env))
+					return (put_err(ERR_EXEC, argv[0]));	
+				return (0);
+			} else {
+				waitpid(pid, NULL, 0);
+				close(p[1]);
+				fd_in = p[0];
+    		}
 			k=l+1;
 		}
 		i=j+1;
--- microshell/test.sh
+++ microshell/test.sh
@@ -1,29 +1,5 @@
-gcc microsh.c -o microshell -Wall -Wextra -Werror -fsanitize=address -g3
-./microshell /bin/ls
-./microshell /bin/cat microshell.c
-./microshell /bin/ls microshell.c
-./microshell /bin/ls salut
-./microshell ";"
-./microshell ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";"
-./microshell ";" ";" /bin/echo OK
-./microshell ";" ";" /bin/echo OK ";"
-./microshell ";" ";" /bin/echo OK ";" ";"
-./microshell ";" ";" /bin/echo OK ";" ";" ";" /bin/echo OK
-./microshell /bin/ls "|" /usr/bin/grep microshell
-./microshell /bin/ls "|" /usr/bin/grep microshell "|" /usr/bin/grep micro
-./microshell /bin/ls "|" /usr/bin/grep microshell "|" /usr/bin/grep micro "|" /usr/bin/grep shell "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro
-./microshell /bin/ls "|" /usr/bin/grep microshell "|" /usr/bin/grep micro "|" /usr/bin/grep shell "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep micro "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell "|" /usr/bin/grep shell
-./microshell /bin/ls ewqew "|" /usr/bin/grep micro "|" /bin/cat -n ";" /bin/echo dernier ";" /bin/echo
-./microshell /bin/ls "|" /usr/bin/grep micro "|" /bin/cat -n ";" /bin/echo dernier ";" /bin/echo ftest ";"
-./microshell /bin/echo ftest ";" /bin/echo ftewerwerwerst ";" /bin/echo werwerwer ";" /bin/echo qweqweqweqew ";" /bin/echo qwewqeqrtregrfyukui ";"
-./microshell /bin/ls ftest ";" /bin/ls ";" /bin/ls werwer ";" /bin/ls microshell.c ";" /bin/ls subject.fr.txt ";"
-./microshell /bin/ls "|" /usr/bin/grep micro ";" /bin/ls "|" /usr/bin/grep micro ";" /bin/ls "|" /usr/bin/grep micro ";" /bin/ls "|" /usr/bin/grep micro ";"
-./microshell /bin/cat reserv_microsh.c "|" /usr/bin/grep a "|" /usr/bin/grep b ";" /bin/cat reserv_microsh.c ";"
-./microshell /bin/cat reserv_microsh.c "|" /usr/bin/grep a "|" /usr/bin/grep w ";" /bin/cat reserv_microsh.c ";"
-./microshell /bin/cat reserv_microsh.c "|" /usr/bin/grep a "|" /usr/bin/grep w ";" /bin/cat reserv_microsh.c
-./microshell /bin/cat reserv_microsh.c ";" /bin/cat reserv_microsh.c "|" /usr/bin/grep a "|" /usr/bin/grep b "|" /usr/bin/grep z ";" /bin/cat reserv_microsh.c
-./microshell ";" /bin/cat reserv_microsh.c ";" /bin/cat reserv_microsh.c "|" /usr/bin/grep a "|" /usr/bin/grep b "|" /usr/bin/grep z ";" /bin/cat reserv_microsh.c
-./microshell blah "|" /bin/echo OK
-./microshell blah "|" /bin/echo OK ";"
-./microshell ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" blah "|" /bin/echo OK ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";" ";"
-rm -rf microshell.dSYM microshell
+./bash_test.sh > ../f1 2> ../f2
+./microshell_test.sh > ../f11 2> ../f22
+diff ../f1 ../f11
+diff ../f2 ../f22
+#rm ../f1 ../f2
